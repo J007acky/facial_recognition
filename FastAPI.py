7fe11849573a137db1aaa0fdcd90ccbe03b80184
J007acky <111886827+J007acky@users.noreply.github.com>
2024-03-24 09:18:53 +0530

    Update FastAPI.py

--- FastAPI.py
+++ FastAPI.py
@@ -5,6 +5,7 @@ import face_recognition
 import pickle
 import cv2
 import pyrebase
+import os
 
 
 # Initialize Firebase using Pyrebase
@@ -22,11 +23,15 @@ config = {
 firebase = pyrebase.initialize_app(config)
 storage = firebase.storage()
 
-
-storage.child().download("Faces/pkl/face_encodings.pkl","face_encodings.pkl")
+# Define the folder containing face images in the Firebase Storage bucket
+storage_folder = "Faces/"
 
 
 app = FastAPI()
+
+class ImgSave(BaseModel):
+    image_url: HttpUrl
+    user_name: str
 
 class ImgInput(BaseModel):
     image_url: HttpUrl
@@ -35,6 +40,8 @@ class ImgOutput(BaseModel):
     label: str
 
 def recognize_face(image_url: HttpUrl) -> ImgOutput:
+
+    storage.child().download("Faces/pkl/face_encodings.pkl","face_encodings.pkl")
     # Downloading image
     response = requests.get(image_url)
     with open("examp.jpg", 'wb') as file:
@@ -57,6 +64,8 @@ def recognize_face(image_url: HttpUrl) -> ImgOutput:
         # Compare the new face encoding to the stored encodings
         results = face_recognition.compare_faces(face_encodings, new_face_encoding[0])
 
+        os.remove("examp.jpg")
+
         for i, result in enumerate(results):
             if result:
                 return ImgOutput(label=labels[i])
@@ -65,7 +74,48 @@ def recognize_face(image_url: HttpUrl) -> ImgOutput:
 
 
 
+def add_face(image_url: HttpUrl,user_name : str) -> ImgOutput:
+    # Downloading image
+    response = requests.get(image_url)
+    with open("examp.jpg", 'wb') as file:
+        file.write(response.content)
+
+    # Load the stored face encodings and labels from the pickle file
+    with open("face_encodings.pkl", "rb") as file:
+        data = pickle.load(file)
+        face_encodings = data["encodings"]
+        labels = data["labels"]
+
+    # Load a new image you want to recognize
+    new_image = cv2.imread("examp.jpg")
+
+    # Convert the BGR image to RGB
+    rgb_img = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
+
+    encode = face_recognition.face_encodings(new_image)[0]
+    face_encodings.append(encode)
+    labels.append(user_name)
+
+    # Delete the temporary downloaded image
+    os.remove("examp.jpg")
+
+    # Save the encodings and labels to a pickle file
+    data = {"encodings": face_encodings, "labels": labels}
+    with open("face_encodings.pkl", "wb") as file:
+        pickle.dump(data, file)
+
+    # Upload the pickle file to Firebase Storage
+    pkl_blob = storage.child(f"{storage_folder}pkl/face_encodings.pkl")
+    pkl_blob.put("face_encodings.pkl")
+
+
 @app.post('/')
 async def scoring_endpoint(item:ImgInput):
     result = recognize_face(item.image_url)
     return result
+
+
+@app.post('/user/')
+async def scoring_endpoint(item:ImgSave):
+    add_face(item.image_url, item.user_name)
+    return ImgOutput(label="User Saved")
